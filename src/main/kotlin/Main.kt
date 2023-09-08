d9998df36b8fd13bfe25eccec90e39a9f7168d1d
ДмитриенкоВВ <vddjack2008@yandex.ru>
2023-09-08 17:32:00 +0300

    1st.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,29 @@
 fun main(args: Array<String>) {
-    val stringOut1 = "Hello World again!";
+    val stringOut1 = "Hello World again!"
     val stringOut2: String = "Hello World again! Triple!";
     val numerousInt = 19;
     val numerousLong: Long = 19;
-    val numerousFloat = 19;
+    val numerousFloat: Float = 19F;
+    val numerousDouble: Double = 19.0;
 
-    second();
-    third(stringOut1);
-    third(stringOut2);
-    fourth(numerousInt);
+    printVoid();
+    printString(stringOut1);
+    printString(stringOut2);
+    printInt(numerousInt);
+    printLong(numerousLong);
+    printFloat(numerousFloat);
+    printDouble(numerousDouble);
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 }
 
-fun second () {println("Hello World!")}
+fun printVoid () {println("Hello World!")}
 
-fun third (stringInput: String) {println(stringInput);}
-fun fourth (numInput: Int) {println(numInput);
+fun printString (stringInput: String) {println("String: $stringInput");}
+fun printInt (numInput: Int) {println("Int: + $numInput");}
 
-}
+fun printLong(numInput: Long) {println("Long: + $numInput");}
+fun printFloat(numInput: Float) {println("Float: + $numInput");}
+fun printDouble(numInput: Double) {println("Double + $numInput");}
